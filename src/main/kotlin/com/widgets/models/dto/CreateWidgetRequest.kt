3cf835901cf317b7cd51d038736e43ba8b889168
Michael Vaughan <mike.vaughan@qonceptual.com>
2016-08-14 21:14:30 -0400

    Removed unnecessary source files.

--- src/main/kotlin/com/widgets/models/dto/CreateWidgetRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.widgets.models.dto
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-/**
- * @author Michael Vaughan
- */
-data class CreateWidgetRequest(@JsonProperty("name") val name: String?, @JsonProperty("color") val color: String?)
--- src/main/resources/db/migration/V1__create_widget_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `Widget` (
-  `id` varchar(36) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `color` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
